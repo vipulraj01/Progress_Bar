fd494c8f30d8420b52f99670ed4c89bad5f6f19c
Abhishek Raj <90976957+abhiraj9708@users.noreply.github.com>
2022-12-09 00:45:00 +0530

    Update progressbar.py

--- progressbar.py
+++ progressbar.py
@@ -1,7 +1,7 @@
 import time
 for i in range(10):
     time.sleep(.1)
-from tqdm import tqdm
+from tqdm import tqdm # tqdm is a library in python which is used for creating Progress Meters or Progress Bars
 import time
 for i in tqdm(range(10)):
     time.sleep(.1)
